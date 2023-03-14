a3ea101f8620435e8d5cf3686665783a5aa578b8
Lucas Vinicius <100610137+LucasVinicius32@users.noreply.github.com>
2023-03-14 16:11:36 -0300

    Sistema de login

--- src/main/java/com/pro4tech/controle/Controle.java
+++ src/main/java/com/pro4tech/controle/Controle.java
@@ -1,10 +1,21 @@
 package com.pro4tech.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.pro4tech.modelo.Funcionario;
+import com.pro4tech.repositorio.RepositorioFuncionario;
+
+import ch.qos.logback.core.model.Model;
 
 @Controller
 public class Controle {
+
+    @Autowired
+    private RepositorioFuncionario rep;
 
     @GetMapping("/")
     public String index(){
@@ -14,6 +25,18 @@ public class Controle {
     @GetMapping("/login")
     public String login(){
         return "login";
+    }
+
+    @PostMapping("/logar")
+    public String logar(@ModelAttribute Funcionario funcionario){
+        String email = funcionario.getEmail();
+        String senha = funcionario.getSenha();
+        Funcionario funcionarioLogado = rep.findByEmailAndSenha(email, senha);
+
+        if(funcionarioLogado != null){
+            return "redirect:/home";
+        }
+        return "redirect:/login";
     }
 
     @GetMapping("/home")
