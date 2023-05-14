63ee9080d339fa857424c213da08818e22b98402
Valderi Douglas <valderi.douglas.1000@gmail.com>
2023-05-13 23:24:27 -0300

    findbyid

--- src/main/java/com/fatec/pro4tech/repository/RepositorioParcela.java
+++ src/main/java/com/fatec/pro4tech/repository/RepositorioParcela.java
@@ -10,18 +10,12 @@ import com.fatec.pro4tech.entities.Parcela;
 
 public interface RepositorioParcela extends JpaRepository<Parcela, Long> {
 
+   Optional<Parcela> findById(Long id_parcela);
+
    @Query("SELECT p FROM Parcela p WHERE p.titulo.id = ?1")
    Optional<List<Parcela>> findByTituloId(Long idTitulo);
 
-<<<<<<< HEAD
    
-   Optional<Parcela> findById(Long id_parcela);
    // @Query("SELECT p FROM Parcela p WHERE p.id_parcela = ?1")
    // Optional<Parcela> findOne(Long id_parcela);
 }
-=======
-   // Optional<Parcela> findByid_parcela (Long id_parcela);
-
-
-}
->>>>>>> ac97f5a7b62036e2e968882608174ab37a3333da
